85ca8cb468943c45bb1e767b218491744422d1c9
Anthony R <70669368+aorom1997@users.noreply.github.com>
2020-11-11 21:30:30 -0500

    Initial commit

---
+++ app/src/main/java/com/example/finalproject/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.finalproject
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val buttonSpeed= findViewById<Button>(R.id.SpeedMode)
+        buttonSpeed.setOnClickListener {
+            val intent = Intent( this , SpeedMode::class.java)
+            startActivity(intent);
+        }
+
+        val buttonSetttings= findViewById<Button>(R.id.Settings)
+        buttonSetttings.setOnClickListener {
+            val intent = Intent( this , Settings::class.java)
+            startActivity(intent);
+        }
+
+        val buttonGame= findViewById<Button>(R.id.Tower)
+        buttonGame.setOnClickListener {
+            val intent = Intent( this , TowerPlaceholder::class.java)
+            startActivity(intent);
+        }
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/finalproject/TowerPlaceholder.kt
@@ -0,0 +1,11 @@
+package com.example.finalproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TowerPlaceholder : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tower_placeholder)
+    }
+}
